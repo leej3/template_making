810930727d896135540b2a2b8ac4cd45d3763431
leej3 <johnleenimh@gmail.com>
2018-05-01 21:25:48 -0400

    rename package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='scripts',
+setup(name='afnipython',
       version='0.0.1',
       description='AFNI python packages',
       url='git+https://github.com/leej3/template_making.git',
       author='AFNI team',
       author_email='afni.bootcamp@gmail.com',
       license='Public Domain',
-      packages=['scripts'],
+      packages=['afnipython'],
       install_requires=[
           'numpy'
       ],
